4a89dfead94d09eb5aae262e5ca1027553231871
rennolds <46907691+rennolds@users.noreply.github.com>
2019-07-25 15:28:25 -0400

    removed code

--- Blackjack_Project/Blackjack_Project/Source.cpp
+++ Blackjack_Project/Blackjack_Project/Source.cpp
@@ -6,27 +6,9 @@ using namespace std;
 
 int main()
 {
-	struct person {
-		string name;
-		int age;
-	};
 	
-	
-	person personNumberOne;
-	personNumberOne.name = "Tommy";
-	personNumberOne.age = 22;
+	cout << "Blacjack Group 7 Represent" << endl;
 
-	person personNumberTwo;
-	personNumberTwo.name = "Scottie";
-	personNumberTwo.age = 18;
-
-	stack<person> stackOfPeople;
-	stackOfPeople.push(personNumberOne);
-	stackOfPeople.push(personNumberTwo);
-
-	cout << stackOfPeople.top().name << " " << stackOfPeople.top().age << endl;
-	stackOfPeople.pop(); 
-	cout << stackOfPeople.top().name << " " << stackOfPeople.top().age << endl;
 
 	system("pause");
 	return 0;
