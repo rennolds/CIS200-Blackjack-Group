d35493c588efc8dffae982c766bbe7a8828f554c
rennolds <46907691+rennolds@users.noreply.github.com>
2019-08-07 00:22:41 -0400

    added class comments

--- Blackjack_Project/Blackjack_Project/Blackjack.h
+++ Blackjack_Project/Blackjack_Project/Blackjack.h
@@ -1,1 +1,83 @@
 #pragma once
+
+#include<vector>
+#include"Player.h"
+#include"deck.h"
+
+
+using namespace std;
+
+class Blackjack 
+{
+
+private:
+	vector<Player> players;
+	Player computerPlayer;
+	Deck gameDeck;
+
+public:
+
+	Blackjack(int numberOfPlayers)
+	{
+		for (int index = 0; index < numberOfPlayers; index++)
+		{
+			Player newPlayer;
+			players.push_back(newPlayer);
+		}
+
+		Player computerPlayer;
+		Deck gameDeck;
+	}
+
+	void play()
+	{
+		while (players.size() > 0)
+		{
+			round();
+		}
+	}
+
+
+	void round()
+	{
+		for (int index = 0; index < players.size(); index++)
+		{
+		 
+			// deal each player 
+			
+		}
+		// deal the dealer 
+
+		for (int index = 0; index < players.size(); index++)
+		{
+
+			// get each players action
+
+		}
+		// get dealer action
+
+		for (int index = 0; index < players.size(); index++)
+		{
+			// pay out each player
+		}
+		// pay out dealer 
+
+		for (int index = 0; index < players.size(); index++)
+		{
+			// remove any player that has busted from the vector
+		}
+	}
+
+	void turn(Player player)
+	{
+
+	}
+
+	void payOut()
+	{
+
+
+	}
+
+
+};
--- Blackjack_Project/Blackjack_Project/Player.h
+++ Blackjack_Project/Blackjack_Project/Player.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include<vector>
+
+using namespace std;
+
+class Player 
+{
+private:
+	bool busted;
+
+public:
+	Player()
+	{
+		busted = false;
+	}
+
+	bool getBusted()
+	{
+		return busted;
+	}
+
+	void bustPlayer()
+	{
+		busted = true;
+	}
+
+
+};
